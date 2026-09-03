17552034bb2fe7c7fc30dffa868a2f17c514cd03
Raquel Guimarães <raqueltodescatto@gmail.com>
2026-09-03 17:18:58 -0300

    Personaliza o cabeçalho do sistema

--- src/Principal.java
+++ src/Principal.java
@@ -1,29 +1,39 @@
 import java.util.Scanner;
+
 public class Principal {
- public static void main(String[] args) {
- Scanner entrada = new Scanner(System.in);
- int opcao;
- do {
- exibirCabecalho(); // <== METODO COMPARTILHADO (ponto de conflito)
- exibirMenu();
- opcao = entrada.nextInt();
- switch (opcao) {
- case 1: /* funcionalidade do Desenvolvedor A */ break;
- case 2: /* funcionalidade do Desenvolvedor B */ break;
- case 0: System.out.println("Encerrando..."); break;
- default: System.out.println("Opcao invalida!");
- }
- } while (opcao != 0);
- entrada.close();
- }
- public static void exibirCabecalho() {
- System.out.println("==============================");
- System.out.println(" NOME DO SISTEMA ");
- System.out.println("==============================");
- }
- public static void exibirMenu() {
- System.out.println("1 - ...");
- System.out.println("0 - Sair");
- System.out.print("Escolha: ");
- }
+	public static void main(String[] args) {
+		 Scanner scan = new Scanner(System.in);
+		 int opcao;
+		 do {
+			 exibirCabecalho(); // <== METODO COMPARTILHADO (ponto de conflito)
+			 exibirMenu();
+			 opcao = scan.nextInt();
+			 switch (opcao) {
+			 	case 1: System.out.println("Coleta de dados"); /* funcionalidade do Desenvolvedor A */ 
+			 		break;
+			 	case 2: System.out.println("Classificação, categoria e relatório");/* funcionalidade do Desenvolvedor B */ 
+			 		break;
+			 	case 0: System.out.println("Encerrando..."); 
+			 		break;
+			 default: System.out.println("Opcao invalida!");
+			 				}
+		 } while (opcao != 0);
+		 scan.close();
+	}
+		 public static void exibirCabecalho() {
+			 System.out.println("==============================");
+			 System.out.println(" Cálculo do IMC");
+			 System.out.println("==============================");
+	}
+		 public static void exibirMenu() {
+			 System.out.println("1 - Coleta de dados.");
+			 System.out.println("2 - Classificação, categoria e relatório");
+			 System.out.println("0 - Sair");
+			 System.out.print("Escolha: ");
+	}
+		 
+		 public static void coletaDados() {
+			 
+	} 
+		 
 }
